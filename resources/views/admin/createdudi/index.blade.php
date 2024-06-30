34cfae53175a2a8e07f0465519279f35bac7668e
ivlalbima07 <ivlalbima07@gmail.com>
2024-06-30 12:41:41 +0700

    tambahan edit dudi

--- resources/views/admin/createdudi/index.blade.php
+++ resources/views/admin/createdudi/index.blade.php
@@ -305,6 +305,227 @@
 
     </div>
 
+    <div class="modal fade text-start" id="edit" aria-labelledby="myModalLabel16" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel16">edit Dudi</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col mb-3">
+                            <label for="nameBasic" class="form-label">Nama Rombel</label>
+                            <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        </div>
+                    </div>
+                    <div class="row g-4">
+                        <div class="col mb-0">
+                            <label for="nameBasic" class="form-label">Nama</label>
+                            <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        </div>
+                        <div class="col mb-0">
+                            <label for="nameBasic" class="form-label">NIB</label>
+                            <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        </div>
+                        <div class="col mb-0">
+                            <label for="emailBasic" class="form-label">Tanggal Terbit / SK Pendirian</label>
+                            <input type="date" class="form-control invoice-edit-input date-picker" />
+                        </div>
+                        <div class="col mb-0">
+                            <label for="nameBasic" class="form-label">Tipe</label>
+                            <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        </div>
+                    </div>
+                    <div class=" my-1">
+                        <label for="label-form">Nama Peserta Lain </label>
+                        <textarea type="text" class="form-control"> </textarea>
+
+                    </div>
+                    <div class="row g-4">
+                        <div class="col mb-0">
+                            <label for="province" class="form-label">Provinsi</label>
+                            <select class="select2 form-select" id="province">
+                                <option value="" hidden>Pilih Provinsi</option>
+                                @foreach ($provinces as $item)
+                                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col mb-0">
+                            <label for="regency" class="form-label">Kabupaten/Kota</label>
+                            <select class="select2 form-select" id="regency">
+                                <option value="" hidden>Pilih Kabupaten</option>
+                            </select>
+                        </div>
+                        <div class="col mb-0">
+                            <label for="district" class="form-label">Kecamatan</label>
+                            <select class="select2 form-select" id="district">
+                                <option value="" hidden>Pilih Kecamatan</option>
+                            </select>
+                        </div>
+                        <div class="col mb-0">
+                            <label for="village" class="form-label">Desa</label>
+                            <select class="select2 form-select" id="village">
+                                <option value="" hidden>Pilih Desa</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row g-2 my-1">
+                        <div class="col mb-0">
+                            <label for="nameBasic" class="form-label">Email Mitra</label>
+                            <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        </div>
+                        <div class="col mb-0">
+                            <label for="nameBasic" class="form-label">No. Telp. Mitra</label>
+                            <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name" />
+                        </div>
+                    </div>
+                    <div>
+                        <label class="form-label" for="select2-multiple">Klasifikasi Baku Lapangan Usaha (KBLI)</label>
+                        <select class="select2 form-select" id="select2-multiple" multiple>
+                            <optgroup label="Alaskan/Hawaiian Time Zone">
+                                <option value="AK">Alaska</option>
+                                <option value="HI">Hawaii</option>
+                            </optgroup>
+                            <optgroup label="Pacific Time Zone">
+                                <option value="CA">California</option>
+                                <option value="NV">Nevada</option>
+                                <option value="OR">Oregon</option>
+                                <option value="WA">Washington</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                    <div class="my-1">
+                        <label class="form-label" for="basicSelect1">Lingkup Kerjasama </label>
+                        <select name="" id="" class="form-select">
+                            <option value="" hidden>Pilih Lingkup Kerjasama</option>
+                            <option class="dropdown-item">Nasional</option>
+                            <option class="dropdown-item">Internasional</option>
+                        </select>
+                    </div>
+                    <div class="my-1">
+                        <label class="form-label" for="basicSelect1">Kriteria Mitra</label>
+                        <select name="" id="" class="form-select">
+                            <option value="" hidden>Pilih Kriteria Mitra</option>
+                            <option class="dropdown-item">Nasional</option>
+                            <option class="dropdown-item">Internasional</option>
+                        </select>
+                    </div>
+                    <div class="my-1">
+                        <label class="form-label" for="basicSelect1">Klasifikasi</label>
+                        <select name="" id="" class="form-select">
+                            <option value="" hidden>Pilih Klasifikasi</option>
+                            <option class="dropdown-item">Nasional</option>
+                            <option class="dropdown-item">Internasional</option>
+                        </select>
+                    </div>
+                    <hr>
+                    <h4 class="text-center">Data Penanggung Jawab</h4>
+                    <hr>
+                    <div class="row">
+                        <!-- Invoice repeater -->
+                        <div class="col-12 my-1">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title text-center">Data Penanggung Jawab</h4>
+                                </div>
+                                <div class="card-body">
+                                    <form action="#" class="invoice-repeater">
+                                        <div data-repeater-list="invoice">
+                                            <div data-repeater-item>
+                                                <div class="row g-4 mb-1">
+                                                    <div class="col mb-0">
+                                                        <label for="nameBasic" class="form-label">Nama</label>
+                                                        <input type="text" id="nameBasic" class="form-control"
+                                                            placeholder="Enter Name" />
+                                                    </div>
+                                                    <div class="col mb-0">
+                                                        <label for="nameBasic" class="form-label">Email</label>
+                                                        <input type="email" id="nameBasic" class="form-control"
+                                                            placeholder="Enter Name" />
+                                                    </div>
+                                                    <div class="col mb-0">
+                                                        <label for="nameBasic" class="form-label">Nomor Hp</label>
+                                                        <input type="text" id="nameBasic" class="form-control"
+                                                            placeholder="Enter Name" />
+                                                    </div>
+                                                    <div class="col mb-0">
+                                                        <button class="btn btn-outline-danger text-nowrap px-1"
+                                                            data-repeater-delete type="button">
+                                                            <i data-feather="x" class="me-25"></i>
+                                                            <span>Delete</span>
+                                                        </button>
+                                                    </div>
+
+                                                </div>
+                                                <div class="row g-4 ">
+                                                    <div class="col mb-0">
+                                                        <label class="form-label" for="basicSelectGender">Jenis
+                                                            Kelamin</label>
+                                                        <select class="form-select" id="basicSelectGender"
+                                                            data-bs-toggle="pill" aria-expanded="true">
+                                                            <option value="" hidden>Pilih Jenis Kelamin</option>
+                                                            <option class="dropdown-item">Laki-Laki</option>
+                                                            <option class="dropdown-item">Perempuan</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="col mb-0">
+                                                        <label class="form-label" for="basicSelectIdentity">Jenis
+                                                            Identitas</label>
+                                                        <select class="form-select" id="basicSelectIdentity"
+                                                            data-bs-toggle="pill" aria-expanded="true">
+                                                            <option value="" hidden>Pilih Jenis Identitas</option>
+                                                            <option class="dropdown-item">Ktp</option>
+                                                            <option class="dropdown-item">Paspor</option>
+                                                            <option class="dropdown-item">Lainnya</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="col mb-0">
+                                                        <label for="nameBasic" class="form-label">Nomor Identitas</label>
+                                                        <input type="text" id="nameBasic" class="form-control"
+                                                            placeholder="Enter Name" />
+                                                    </div>
+                                                    <div class="col mb-0">
+                                                        <label class="form-label"
+                                                            for="basicSelectNationality">Kewarganegaraan</label>
+                                                        <select class="form-select" id="basicSelectNationality"
+                                                            data-bs-toggle="pill" aria-expanded="true">
+                                                            <option value="" hidden>Pilih Kewarganegaraan</option>
+                                                            <option class="dropdown-item">WNI</option>
+                                                            <option class="dropdown-item">WNA</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <hr />
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <button class="btn btn-icon btn-primary" type="button"
+                                                    data-repeater-create>
+                                                    <i data-feather="plus" class="me-25"></i>
+                                                    <span>Add New</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Invoice repeater -->
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Accept</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
       {{-- modal view --}}
     <div class="modal fade text-start" id="view" tabindex="-1" aria-labelledby="myModalLabel16"
         aria-hidden="true">
